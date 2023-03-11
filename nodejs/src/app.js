77469a1b89c69238b30d0011a10246415f8e8c1b
Thandraw <dangquocthanh2812@gmail.com>
2023-03-11 22:44:25 +0700

    second change

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const express = require('express')//thu vien
-var morgan = require('morgan');
-var app = express();
+const morgan = require('morgan');
+const app = express();
 const port = 3000;
-var handlebars = require('express-handlebars');
+const handlebars = require('express-handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 // HTTP logger
 app.use(morgan('combined'));
-
 app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
 app.engine('.hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
@@ -19,12 +18,19 @@ app.set('views', path.join(__dirname, 'resources/views'));
 //route
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('Okie');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
-})
+});
